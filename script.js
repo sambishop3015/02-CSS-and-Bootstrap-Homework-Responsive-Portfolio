52f01b3d6dc8fb2732cb97b6004a776b4edbb059
Sam Bishop <sambishop3015@hotmail.com>
2021-01-11 20:12:22 -0500

    Reversed Parallax, Added About Me, Added Portfolio Images

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', ()=> {
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+    
+    if (scrolled === scrollable) {
+        alert("You've reached the bottom!")
+    }
+    console.log(scrolled);
+})
+
+// Page scroll on button click feature. NOT WORKING!!!!
+$('#testBtn').click(function(e){
+    $('body').animate({scrollTop:0}, 400);
+    $('.sectionOne').slideDown(300, function(){
+        $('.openPopup').fadeIn(300);
+    });
+    e.preventDefault();
+    console.log("this far...")
+});
+// $('.close').click(function(e){
+//     // $('.openPopup').fadeOut(300, function(){
+//         $('.sectionOne').slideUp(300);
+//     });
+//     e.preventDefault();
+// // });
