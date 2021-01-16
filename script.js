e241fb043f88c9e3d1dbfb0fed75ef7f3e552766
Sam Bishop <sambishop3015@hotmail.com>
2021-01-16 14:22:54 -0500

    Added Contact Page and Form

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
@@ -20,7 +19,7 @@ $('a[href*="#"]')
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 50, function() {
+        }, 50, function () {
           // Callback after animation
           // Must change focus!
           var $target = $(target);
@@ -28,10 +27,18 @@ $('a[href*="#"]')
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
       }
     }
   });
+
+// CONTACT FORM SUBMISSION
+function redirect() {
+  // NOT GATHERING CONTACT INFO YET
+  alert("Thanks for your submission! We'll be in contact soon.");
+  window.location.replace("index.html");
+  return false;
+}
